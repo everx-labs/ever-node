29a3af61d3b8651d1ec991522b44912b500c42af
SergeyY <43989533+yaroslavser@users.noreply.github.com>
2024-02-01 18:19:21 +0300

    Update block version

--- src/validating_utils.rs
+++ src/validating_utils.rs
@@ -1,5 +1,5 @@
 /*
-* Copyright (C) 2019-2021 TON Labs. All Rights Reserved.
+* Copyright (C) 2019-2024 EverX. All Rights Reserved.
 *
 * Licensed under the SOFTWARE EVALUATION License (the "License"); you may not use
 * this file except in compliance with the License.
@@ -54,7 +54,7 @@ pub fn supported_capabilities() -> u64 {
 }
 
 pub fn supported_version() -> u32 {
-    47
+    48
 }
 
 pub fn check_this_shard_mc_info(
